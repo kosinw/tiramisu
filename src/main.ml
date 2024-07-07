30f256cda4c324a4489a0497f124749ee6bfe246
Kosi Nwabueze <kosinw@mit.edu>
2024-07-07 05:36:45 -0400

    Added parse subcommand to tiramisu binary

--- src/main.ml
+++ src/main.ml
@@ -3,7 +3,7 @@ open! Tiramisu
 
 let lex =
   Command.basic
-    ~summary:"runs the lexer and prints out tokens to stdout"
+    ~summary:"runs the lexer and prints out token stream"
     (let open Command.Let_syntax in
      let%map_open filename =
        anon (maybe_with_default "-" ("filename" %: Filename_unix.arg_type))
@@ -21,11 +21,37 @@ let lex =
        In_channel.close channel)
 ;;
 
+let parse =
+  Command.basic
+    ~summary:"runs the parse and prints out the syntax tree"
+    (let open Command.Let_syntax in
+     let%map_open filename =
+       anon (maybe_with_default "-" ("filename" %: Filename_unix.arg_type))
+     in
+     fun () ->
+       let channel =
+         match filename with
+         | "-" -> In_channel.stdin
+         | _ -> In_channel.create filename
+       in
+       let lexer = Lexer.from_channel ~filename channel in
+       let parser = Parser.parse lexer in
+       let syntax = Parser.syntax parser in
+       let spans = Parser.spans parser in
+       let errors = Parser.errors parser in
+       print_s [%message (syntax : Syntax.t)];
+       print_s [%message (spans : Span.t Id.Map.t)];
+       print_s [%message (errors : (Error.t * Position.t) list)];
+       In_channel.close channel)
+;;
+
 let readme () =
   "Tiramisu is a compiler for an OCaml subset which targets RISC-V assembly. It can also \
    run individual compilation stages for analysis or debugging purposes."
 ;;
 
 let () =
-  Command_unix.run ~version:"1.0" (Command.group ~summary:"" [ "lex", lex ] ~readme)
+  Command_unix.run
+    ~version:"1.0"
+    (Command.group ~summary:"" [ "lex", lex; "parse", parse ] ~readme)
 ;;
